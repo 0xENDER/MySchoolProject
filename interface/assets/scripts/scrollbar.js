158f21eaf0883efccfd81cdaf56d78a0990f939b
EnderAdel <72353925+adel-sbeh@users.noreply.github.com>
2021-09-09 20:04:27 +0300

    Add a scrollbar

---
+++ interface/assets/scripts/scrollbar.js
@@ -0,0 +1,13 @@
+/*
+    Credit: https://jsfiddle.net/uq2pLufn/3/
+*/
+
+var scrollbar = {
+    container: document.getElementById('scrollbar'),
+    handle: document.getElementById('handle'),
+    linkedElement: document.getElementById('page'),
+    content: document.querySelectorAll('#page > *:not(#scroll)')
+}
+
+
+container.scrollTo(0, 20);
