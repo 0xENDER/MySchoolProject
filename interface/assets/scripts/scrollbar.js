59840135b4e821e9a503a681f1ac7c248fea69c2
EnderAdel <72353925+adel-sbeh@users.noreply.github.com>
2021-09-09 21:45:52 +0300

    Add a scrollbar

--- interface/assets/scripts/scrollbar.js
+++ interface/assets/scripts/scrollbar.js
@@ -2,12 +2,35 @@
     Credit: https://jsfiddle.net/uq2pLufn/3/
 */
 
-var scrollbar = {
-    container: document.getElementById('scrollbar'),
-    handle: document.getElementById('handle'),
-    linkedElement: document.getElementById('page'),
-    content: document.querySelectorAll('#page > *:not(#scroll)')
+function linkScrollbar() { // Link a scrollbar to the page
+
+    // Get all the elements to start setting up the scrollbar
+
+    var scrollbar = {
+        container: document.getElementById('scrollbar'), // The handle container
+        handle: document.getElementById('scrollbar--handle'), // The handle
+        topButton: document.getElementById('scrollbar--top'), // The "scroll top" button
+        bottomButton: document.getElementById('scrollbar--bottom'), // The "scroll bottom" button
+        linkedElement: document.getElementById('page') // The page content element
+    };
+
+    scrollbar.handle.style.height = // Change the height of the scrollbar handle
+        `${(scrollbar.linkedElement.clientHeight/scrollbar.linkedElement.scrollHeight)*100}%`;
+    //      ^ The height of the content element  ^ The scroll-height of the content element
+    scrollbar.linkedElement.onscroll = function() {
+        scrollbar.handle.style.top =
+            `${(scrollbar.linkedElement.scrollTop/scrollbar.linkedElement.scrollHeight)*100}%`;
+        //      ^ The offset top value of the content element
+    };
+
+    // scrollbar.linkedElement.scrollTo(0, yPosition);
+
+    scrollbar.linkedElement.scrollbar = scrollbar; // Link this object to the page content element
+
 }
 
+function unlinkScrollbar(element) { // Unlink a scrollbar from the page
 
-container.scrollTo(0, 20);
+    delete element.scrollbar.linkedElement.onscroll, element.scrollbar;
+
+}
