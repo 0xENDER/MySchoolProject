9371f16f1e877b059cb865c1702d6c70d059b227
EnderAdel <72353925+adel-sbeh@users.noreply.github.com>
2021-09-11 16:26:20 +0300

    Update some files

--- interface/assets/scripts/tab.js
+++ interface/assets/scripts/tab.js
@@ -1,12 +1,23 @@
-/* Detect when the tab button is used, and when it's not! */
+// If this device has a touch screen, you should always enable the outline!
+if (('ontouchstart' in window) ||
+    (navigator.maxTouchPoints > 0) ||
+    (navigator.msMaxTouchPoints > 0)) {
 
-document.addEventListener('mousedown', function() {
-    document.documentElement.dataset.tab = false;
-});
+    document.documentElement.dataset.tab = true;
 
-document.addEventListener('keydown', function(e) {
-    if (e.code === "Tab") {
-        console.log(e);
-        document.documentElement.dataset.tab = true;
-    }
-});
+} else {
+
+    /* Detect when the tab button is used, and when it's not! */
+
+    document.addEventListener('mousedown', function() {
+        document.documentElement.dataset.tab = false;
+    });
+
+    document.addEventListener('keydown', function(e) {
+        if (e.code === "Tab") {
+            console.log(e);
+            document.documentElement.dataset.tab = true;
+        }
+    });
+
+}
