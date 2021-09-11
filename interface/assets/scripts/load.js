214d7b6eeea4be15459b971d60b4e64d440dd79b
EnderAdel <72353925+adel-sbeh@users.noreply.github.com>
2021-09-11 20:15:26 +0300

    Change the way the page loads

--- interface/assets/scripts/load.js
+++ interface/assets/scripts/load.js
@@ -1,3 +1,9 @@
 window.addEventListener('load', function() {
     document.documentElement.dataset.loaded = true;
+    fetch("./test.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("page").innerHTML = data;
+        });
+
 });
