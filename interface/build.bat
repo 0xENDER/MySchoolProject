db0c6c2f7611f8b8b976a88eae5896a137841da2
EnderAdel <72353925+adel-sbeh@users.noreply.github.com>
2021-09-18 10:50:36 +0300

    Add some command files

--- interface/build.bat
+++ interface/build.bat
@@ -1,4 +1,18 @@
 :: Build the website for the web, Windows, Linux, MacOS, Android, and iOS
 
 
-:: Build
+:: Clear the "/builds/" directory
+if exist "builds/" (
+
+    rmdir /S /Q "builds/"
+
+)
+
+:: Recreate the "/builds/" directory
+mkdir "builds/"
+mkdir "builds/web/"
+mkdir "builds/windows/"
+mkdir "builds/linux/"
+mkdir "builds/mac/"
+mkdir "builds/android/"
+mkdir "builds/ios/"
---
+++ interface/build.sh
@@ -0,0 +1,18 @@
+## Build the website for the web, Windows, Linux, MacOS, Android, and iOS
+
+
+## Clear the "/builds/" directory
+if [ -d "builds/" ]; then
+
+    rm -rf "builds/"
+
+fi
+
+## Recreate the "/builds/" directory
+mkdir "builds/"
+mkdir "builds/web/"
+mkdir "builds/windows/"
+mkdir "builds/linux/"
+mkdir "builds/mac/"
+mkdir "builds/android/"
+mkdir "builds/ios/"
