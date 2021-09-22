04ae5b1805c47c393ef0a99825e0e3616b0304ef
EnderAdel <72353925+adel-sbeh@users.noreply.github.com>
2021-09-22 06:32:07 +0300

    Update some NodeJS modules

---
+++ bundler/minifier/minifyImages.js
@@ -0,0 +1,11 @@
+/*
+
+    Minify and compress all the images in the 'apps_codebase/assets/media/' directory
+
+*/
+
+// Get the required modules
+const compress_images = 0,
+    path = require("path");
+
+//
