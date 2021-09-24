f0b7e85eb2896c56c4b2b4baf82110507d51ff1a
EnderAdel <72353925+adel-sbeh@users.noreply.github.com>
2021-09-24 14:31:55 +0300

    Update the bundler

--- bundler/electron.sh
+++ bundler/electron.sh
@@ -7,6 +7,15 @@ BUILD_WINDOWS=$2
 BUILD_LINUX=$3
 BUILD_MAC=$4
 #
+## Navigate to the framework's build directory
+cd "builds/frameworks/electron/"
+#
+## Install all the NodeJS modules
+"./electron/install_modules.sh"
+#
+## Navigate back to the bundler's main directory
+cd "../../../"
+#
 ## <string>
 echo "[Error] The Electron builder isn't ready yet!"
 #
--- bundler/react.sh
+++ bundler/react.sh
@@ -6,6 +6,15 @@ DEBUG_MODE=$1
 BUILD_ANDROID=$2
 BUILD_IOS=$3
 #
+## Navigate to the framework's build directory
+cd "builds/frameworks/react/"
+#
+## Install all the NodeJS modules
+"./react/install_modules.sh"
+#
+## Navigate back to the bundler's main directory
+cd "../../../"
+#
 ## <string>
 echo "[Error] The React builder isn't ready yet!"
 #
