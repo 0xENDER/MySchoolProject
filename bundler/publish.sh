0529337e149e589a4065ed9578a349c8317c68bd
EnderAdel <adel.sbeh.email@gmail.com>
2021-10-12 23:46:57 +0300

    Add a placeholder file

--- bundler/publish.sh
+++ bundler/publish.sh
@@ -21,6 +21,18 @@ fi
 echo -e "\n[Publisher] Cleaning up the 'public/' directory..."
 "./publish/clean.sh" $DEBUG_MODE $LOCAL
 #
+## Check if this is not local
+if [ $LOCAL -eq 0 ]; then
+#
+    ## Stop normal traffic to the website
+    echo -e "\n[Publisher] Freezing the traffic..."
+    "./publish/freeze.sh"
+    #########
+    ##
+    #########
+#
+fi
+#
 ## EXIT!
 echo -e "\n[Publisher] [Error] NOT READY YET!"
 #
