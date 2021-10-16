366b2c8282a870410175af320794e5ae1f577203
EnderAdel <adel.sbeh.email@gmail.com>
2021-10-16 21:34:18 +0300

    Add an `onclick` event to the options button for mobile devices

---
+++ interface/assets/scripts/menu.js
@@ -0,0 +1,22 @@
+/*
+
+    Manage the mobile menu
+
+*/
+
+
+// Define the needed variables
+var optionsButton = document.getElementById("button--menuOptions"),
+    profilePictureButton = document.getElementById("user--profilepicture");
+
+// Show the menu
+function showMobileMenu() {
+
+    alert("Not ready yet!");
+    openSignInRequest();
+
+}
+
+// Update the `onclick` event functions
+optionsButton.onclick = showMobileMenu;
+profilePictureButton.onclick = showMobileMenu;
