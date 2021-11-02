86d69ac9a696e2cec831541d136aad7b28facb8d
EnderAdel <adel.sbeh.email@gmail.com>
2021-11-02 09:45:28 +0200

    Update cover.js

--- interface/pages/home/cover.js
+++ interface/pages/home/cover.js
@@ -10,14 +10,19 @@ function homepageCoverEffect() {
     // Get the required elements
     var homepageCoverBackgroundElement = document.getElementById("homepage--background");
 
+    // Keep updating the CSS properties of the cover on scroll
     pageContentElement.addEventListener("scroll", function() {
 
+        // Get the element's scrolling-related variables
         var scrollTop = pageContentElement.scrollTop,
             clientHeight = pageContentElement.clientHeight,
             clientHeightDiff = clientHeight - scrollTop;
-        homepageCoverBackgroundElement.style.transform = `Scale(${1 + 1/3 - clientHeightDiff / clientHeight / 3})`;
-        homepageCoverBackgroundElement.style.opacity = (clientHeightDiff - 210) / (clientHeight - 210);
 
+        // Update the style attribute
+        homepageCoverBackgroundElement.style.transform = `Scale(${1 + 1/3 - clientHeightDiff / clientHeight / 3})`;
+        homepageCoverBackgroundElement.style.opacity = (clientHeightDiff - 250) / (clientHeight - 250);
+
+        // Delete the used variables
         delete scrollTop, clientHeight, clientHeightDiff;
 
     }, window.performanceVariables.objects.passiveEvent);
