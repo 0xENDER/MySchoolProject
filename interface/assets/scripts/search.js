b8ed2a0e5a9e817842749171c65c527db359e41b
EnderAdel <adel.sbeh.email@gmail.com>
2021-11-03 11:08:46 +0200

    Update the search interface

--- interface/assets/scripts/search.js
+++ interface/assets/scripts/search.js
@@ -8,21 +8,60 @@
 // Get the needed elements
 var searchBackButton = document.getElementById("button--searchback"),
     searchVoiceButton = document.getElementById("button--searchvoice"),
-    searchBar = document.getElementById("component--search");
+    searchBar = document.getElementById("component--search"),
+    searchContainer = document.getElementById("search--container");
 
-// Check if this is a small device that supports touch input
-if (window.platform.hardware.display.isTouchCapable && window.platform.special.dynamic.isWindowSmall()) {
+// Show the search interface
+function showSearch() {
 
-    // Handle the events of the search bar
-    searchBar.onclick = function() {
+    // Check if this device has a small screen
+    if (window.platform.special.dynamic.isWindowSmall()) {
 
+        // Show the mobile search interface
         document.documentElement.dataset.usingSearch = true;
 
-    };
-    searchBar.onblur = function() { // Debug
+    } else {
 
-        document.documentElement.dataset.usingSearch = false;
+        // Show the search container
+        searchContainer.style.display = "block";
 
-    };
+    }
+
+    // Make sure that the search bar is focused
+    searchBar.focus();
 
 }
+
+// Handle the events of the search bar
+searchBar.onmousedown = showSearch;
+searchBar.ontouchstart = showSearch;
+searchBar.onkeyup = function() {
+
+    // Check if the search bar is focused
+    if (document.activeElement == searchBar) {
+
+        // Show the search interface
+        showSearch();
+
+    } else {
+
+        // Hide the mobile search interface
+        document.documentElement.dataset.usingSearch = false;
+
+    }
+
+};
+searchBar.onblur = function() { // Debug
+
+    // Hide the mobile search interface
+    document.documentElement.dataset.usingSearch = false;
+
+    // Check if this device has a big screen
+    if (!window.platform.special.dynamic.isWindowSmall()) {
+
+        // Hide the search container
+        searchContainer.style.display = null;
+
+    }
+
+};
