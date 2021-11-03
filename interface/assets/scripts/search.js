e53a5aba2e69201a64db8a2075462a6d2ec95ea6
EnderAdel <adel.sbeh.email@gmail.com>
2021-11-03 12:03:28 +0200

    Update the search bar interface

--- interface/assets/scripts/search.js
+++ interface/assets/scripts/search.js
@@ -8,8 +8,10 @@
 // Get the needed elements
 var searchBackButton = document.getElementById("button--searchback"),
     searchVoiceButton = document.getElementById("button--searchvoice"),
+    searchClearButton = document.getElementById("button--searchclear"),
     searchBar = document.getElementById("component--search"),
-    searchContainer = document.getElementById("search--container");
+    searchContainer = document.getElementById("search--container"),
+    isUsingSearch = false;
 
 // Show the search interface
 function showSearch() {
@@ -25,10 +27,51 @@ function showSearch() {
         // Show the search container
         searchContainer.style.display = "block";
 
+        // Update the `isUsingSearch` variable
+        isUsingSearch = true;
+
     }
 
     // Make sure that the search bar is focused
     searchBar.focus();
+
+}
+
+// Hide the search interface
+function hideSearch() {
+
+    // Hide the mobile search interface
+    document.documentElement.dataset.usingSearch = false;
+
+    // Check if this device has a big screen
+    if (!window.platform.special.dynamic.isWindowSmall()) {
+
+        // Hide the search container
+        searchContainer.style.display = null;
+
+        // Update the `isUsingSearch` variable
+        isUsingSearch = false;
+
+    }
+
+}
+
+// Update the search button (the voice input and clear buttons)
+function updateSearchButton() {
+
+    // Check if the search input box is empty
+    if (searchBar.value != "") {
+
+        searchVoiceButton.style.display = "none";
+        searchClearButton.style.display = null;
+
+
+    } else {
+
+        searchClearButton.style.display = "none";
+        searchVoiceButton.style.display = null;
+
+    }
 
 }
 
@@ -43,6 +86,9 @@ searchBar.onkeyup = function() {
         // Show the search interface
         showSearch();
 
+        // Update the search buttons
+        updateSearchButton();
+
     } else {
 
         // Hide the mobile search interface
@@ -51,17 +97,65 @@ searchBar.onkeyup = function() {
     }
 
 };
-searchBar.onblur = function() { // Debug
+searchBar.onkeydown = function() {
 
-    // Hide the mobile search interface
-    document.documentElement.dataset.usingSearch = false;
+    // Check if the search bar is focused
+    if (document.activeElement == searchBar) {
 
-    // Check if this device has a big screen
-    if (!window.platform.special.dynamic.isWindowSmall()) {
-
-        // Hide the search container
-        searchContainer.style.display = null;
+        // Update the search buttons
+        updateSearchButton();
 
     }
 
 };
+searchBackButton.onclick = hideSearch;
+
+// Clear the content of the search input box when the clear button is clicked
+searchClearButton.onclick = function() {
+
+    // Update the value of the search bar input box
+    searchBar.value = "";
+
+    // Update the search buttons
+    updateSearchButton();
+
+    // Focus the input field
+    searchBar.focus();
+
+};
+
+// Hide the menu on blur (desktop/big screens)
+if (!window.platform.special.dynamic.isWindowSmall()) {
+
+    // Define a variable to keep track of the clicks
+    var clickInside = true;
+
+    // Detect clicks inside the search container
+    searchContainer.addEventListener("mousedown", function() {
+
+        clickInside = true;
+
+    }, window.performanceVariables.objects.passiveEvent);
+
+    // Detect clicks outside the whole page
+    window.addEventListener("mousedown", function() {
+
+        if (isUsingSearch) {
+
+            // Check if this click was outside the search container
+            if (!clickInside) {
+
+                hideSearch();
+                clickInside = true;
+
+            } else {
+
+                clickInside = false;
+
+            }
+
+        }
+
+    }, window.performanceVariables.objects.passiveEvent);
+
+}
