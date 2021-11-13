6b1b0ff7ff5a410fbec58149ab920771abdbd95d
EnderAdel <adel.sbeh.email@gmail.com>
2021-11-14 01:36:51 +0200

    Correct a comment

--- interface/assets/scripts/contextmenu.js
+++ interface/assets/scripts/contextmenu.js
@@ -10,7 +10,7 @@ window.oncontextmenu = function(event) {
 
     var tagName = event.path[0].tagName;
 
-    // Check if the current element is an input or if it has some selected text inside
+    // Check if the current element is not an input or if it does not have selected text inside
     if (tagName != "INPUT" && !(event.path[0].hasAttribute("enable-select") && window.getSelection().toString() != "")) {
 
         event.preventDefault();
