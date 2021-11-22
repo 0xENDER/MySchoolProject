cabfed33acd3c78ec36f9b0936692aa265d9a5e1
EnderAdel <adel.sbeh.email@gmail.com>
2021-11-22 19:45:05 +0200

    Fix a Firefox-related bug

--- interface/assets/scripts/contextmenu.js
+++ interface/assets/scripts/contextmenu.js
@@ -8,10 +8,11 @@
 // Detect any attempt to access the context menu
 window.oncontextmenu = function(event) {
 
-    var tagName = event.path[0].tagName;
+    // Get the tag name of the target element
+    var tagName = event.target.tagName;
 
     // Check if the current element is not an input or if it does not have selected text inside
-    if (tagName != "INPUT" && !(event.path[0].hasAttribute("enable-select") && window.getSelection().toString() != "")) {
+    if (tagName != "INPUT" && !(event.target.hasAttribute("enable-select") && window.getSelection().toString() != "")) {
 
         event.preventDefault();
 
